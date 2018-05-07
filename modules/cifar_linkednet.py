84e0b8e5ab43b60b5c34d8b0c7f669439a1b9614
Arun Drelich <arun@arundreli.ch>
2018-05-07 19:13:40 -0400

    visualising shit

--- modules/cifar_linkednet.py
+++ modules/cifar_linkednet.py
@@ -1,18 +1,18 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from torch.autograd import Variable
-
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 # Helper functions
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+#    img = img / 2 + 0.5     # unnormalize
+    print("\t #{}".format(img.size()))
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
@@ -20,7 +20,7 @@ def imshow(img):
 data_dir = '../data/CIFAR10'
 v_sz = 10
 learning_rate = 0.01
-batch_sz = 4
+batch_sz = 50
 num_epochs = 1
 
 DEVICE = torch.device('cpu')
@@ -76,24 +76,32 @@ class CIFAR10LinkedNet(nn.Module):
 
     def forward_single_image(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        f = self.conv2(x)
+        x = self.pool(F.relu(f))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return x
-
+        return f,x
 
     def forward(self, x1, x2):
         # make forward call on each
-        out1 = self.forward_single_image(x1)
-        out2 = self.forward_single_image(x2)
+        f1,out1 = self.forward_single_image(x1)
+        f2,out2 = self.forward_single_image(x2)
         # concat
         x = torch.cat((out1, out2), dim=1)
+        f = torch.cat((f1, f2), dim=1)
         # pass through MLP
-        return self.mlp(x)
+        return f, self.mlp(x)
 
-net = CIFAR10LinkedNet()
+def vis_conv1(net):
+    conv1_weights = net.conv1.weight.data.view(-1, 3, 5, 5).cpu()
+    imshow(torchvision.utils.make_grid(conv1_weights, pad_value=1, normalize=True))
+def vis_conv2(net):
+    conv2_weights = net.conv2.weight.data.view(-1, 1, 5, 5).cpu()
+    imshow(torchvision.utils.make_grid(conv2_weights, pad_value=1, normalize=True))
+
+net = CIFAR10LinkedNet(device=DEVICE)
 print(net)
 
 criterion = nn.CrossEntropyLoss()
@@ -101,16 +109,27 @@ optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
 loss_vec = []
 
+vis_conv1(net)
+plt.title("Epoch[0] conv1")
+plt.figure()
+vis_conv2(net)
+plt.title("Epoch[0] conv2")
+plt.figure()
+
 for epoch in range(num_epochs):
     for batch_idx, (images, labels) in enumerate(trainloader):
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+
         images1, images2 = torch.chunk(images, 2, dim=0)
+
         labels1, labels2 = torch.chunk(labels, 2, dim=0)
 
         eq_labels = Variable(labels1.eq(labels2).long())
 
         optimizer.zero_grad()
 
-        out = net(images1, images2)
+        fmap, out = net(images1, images2)
         loss = criterion(out, eq_labels)
 
         loss_vec.append(loss.item())
@@ -120,6 +139,22 @@ for epoch in range(num_epochs):
         if batch_idx % 100 == 0:
             print("Epoch[%d], Step [%d], Total Loss: %.4f"
                    %(epoch+1, batch_idx, loss.item()))
+        if batch_idx == len(trainloader) - 1:
+            print(images.data.size())
+            print(fmap.data.size())
+#            for i in fmap.data.size(0):
+#                f = fmap[i,:,:,:]
+#                imshow(torchvision.utils.make_grid(f.data.view(-1, 1, 10, 10).cpu(), pad_value=1))
+#                plt.show()
+
+    vis_conv1(net)
+    plt.title("Epoch[{}] conv1".format(epoch+1))
+    plt.figure()
+    vis_conv2(net)
+    plt.title("Epoch[{}] conv2".format(epoch+1))
+    plt.figure()
+
 
 plt.plot(loss_vec)
+plt.title("Loss over {} epochs".format(num_epochs))
 plt.show()
