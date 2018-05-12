9d77e05eded1b3202dc5b02b5238da2800e63e11
mitchell <mitchellwortsman@gmail.com>
2018-05-12 16:15:56 -0400

    test

--- pytorch-CycleGAN-and-pix2pix/models/base_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/base_model.py
@@ -84,7 +84,10 @@ class BaseModel():
                 if len(self.gpu_ids) > 0 and torch.cuda.is_available():
                     net.module.load_state_dict(torch.load(save_path))
                 else:
-                    net.load_state_dict(torch.load(save_path))
+                    sd = torch.load(save_path)
+                    del sd['model.10.conv_block.5.weight']
+                    del sd['model.10.conv_block.5.bias']
+                    net.load_state_dict(sd)
 
     # print network information
     def print_networks(self, verbose):
