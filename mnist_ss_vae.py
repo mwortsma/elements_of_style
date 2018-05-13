d27eaaad6ab4bb4c19e0d833ac108544bd007f1d
katie <kathryn_scholl@brown.edu>
2018-05-13 16:31:13 -0400

    visualizer

--- mnist_ss_vae.py
+++ mnist_ss_vae.py
@@ -6,6 +6,8 @@ import torchvision
 import torchvision.transforms as transforms
 
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 import torch.nn as nn
@@ -13,6 +15,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 import modules.ss_vae as ss_vae
+import visualize as vis
 
 # Parameters
 data_dir = 'data/MNIST'
@@ -24,6 +27,14 @@ num_classes = 10
 # Helpers
 def icdf(v):
     return torch.erfinv(2 * torch.Tensor([float(v)]) - 1) * np.sqrt(2)
+
+def horzArr(a):
+    ''' finagles a (num_images)x1x(a)x(b) array into a (a)x(b*num_images) array '''
+    a = np.squeeze(a) # get rid of extra 1d
+    a = np.split(a,np.size(a,0)) # separate images
+    a= np.concatenate(a,2) # put together horizontally
+    a = np.squeeze(a) # get rid of extra 1d
+    return a
 
 def train(vae, data_loader, fixed_x, fixed_y):
     optimizer = torch.optim.Adam(vae.parameters(), lr=learning_rate)
@@ -79,11 +90,11 @@ def main():
     fixed_y = Variable(fixed_y_save.to(DEVICE))
     torchvision.utils.save_image(fixed_x_save, os.path.join(args.res, 'real_images.png'))
 
-    vae = ss_vae.SS_VAE(device=DEVICE)
+    vae = ss_vae.SS_VAE(device=DEVICE, z_sz=z_sz)
     if args.load is None:
         train(vae, data_loader, fixed_x, fixed_y)
     else:
-        vae.load_state_dict(torch.load(args.load))
+        vae.load_state_dict(torch.load(args.load, map_location=lambda storage, loc: storage))
 
         # Save reconstructed image
         reconst_images, _, _ = vae(fixed_x)
@@ -120,14 +131,12 @@ def main():
 
         if args.walk:
             # set up visualizer-- see how changing z alters changes images over all labels y
-            f = lambda z: vae.sample(
-                Variable(torch.from_numpy(z).float().to(DEVICE)),labels).data.numpy().reshape(28, -1)
-                # will this sizing work?
-            np_x = fixed_x[0].view(28,28).data.numpy()
-            mu, logvar = torch.chunk(z_params,2,dim=1)
-            np_mu = mu.data.numpy()
-            np_sigma = torch.exp(logvar/2).data.numpy() # XXX is this right??
-            v = vis.Visualizer(f,np_x,np_mu,np_sigma)
+            f = lambda z:horzArr(vae.sample(
+                    # to get z: convert to torch, replicate 11 times
+                    Variable(torch.from_numpy(z).float().to(DEVICE)).view(1,-1).expand(num_classes+1,-1),
+                    # format to be 11 28x28 images horizontally arranged
+                    labels).view(-1,1,28,28).data.numpy())
+            v = vis.Visualizer(f,z_sz=z_sz)
             v.visualize()
 
 if __name__ == "__main__":
--- visualize.py
+++ visualize.py
@@ -1,50 +1,47 @@
 import numpy as np
-from Tkinter import * # do we want python 2.x? if so, use Tkinter
- import Image, ImageTk
+from tkinter import * # do we want python 2.x? if so, use Tkinter
+import PIL
+from PIL import Image, ImageTk
 #import modules.ss_vae as ss_vae
 
-max_args = 10
+def npToTk(arr):
+    ''' convert np array to imageTk format '''
+    typedArr = (arr * 255).astype(np.uint8)
+    im = Image.fromarray(typedArr)
+    return ImageTk.PhotoImage(im)
 
 class Visualizer:
-    __init__(self, f, x, mu, sigma):
+    def __init__(self, f, window_sz=5, z_sz=50, max_sliders=10):
         '''
         f: single-arg function (only requiring z), outputting an image
-        x: original image, as np array
-        mu, sigma: specifications for distribution to wander around (np arrays)
+        window_sz: how much to each side the sliders can move
+        z_sz: length of z vector
+        max_sliders: max number of sliders to display
         '''
         self.f = f
-        self.x = x
-        self.mu = mu
-        self.sigma = sigma
+        self.max_sliders = max_sliders
+        self.z_sz = z_sz
+        self.window_sz = window_sz
 
         self.root = Tk()
         self.sliders = []
 
-        # out.data.numpy.()
-        # original image in upper left
-        img=Image.fromarray(x)
-        imgTk=ImageTk.PhotoImage(img)
-        original=Label(self.root, image=imgTk)
-        original.image=imgTk
-        original.grid(row=0,column=0)
-
         # will be changed to a new image when sliders change
-        avg = Image.fromarray(f(self.mu)) # sample for when z=mu
-        avgTk = ImageTk.PhotoImage(avg)
-        self.walked=Label(self.root, image=avgTk)
-        self.walked.image=avgTk
-        self.walked.grid(row=0,column=1)
+        avgIm = npToTk(f(np.zeros(self.z_sz)))
+        self.walked=Label(self.root, image=avgIm)
+        self.walked.image=avgIm
+        self.walked.grid(row=0)
 
-    def refresh():
+    def refresh(self,event):
         ''' refresh decoded image using values in sliders '''
-        z = np.array(self.mu) # might not have value for all sliders, bc limited space
-        for i,slider in self.sliders:
-            z[i]=slider.get() # XXX double check that this makes sense
-        img = f(z)
-        self.walked.configure(image=img)
-        self.walked.image=img # prevent garbage collection
+        z = np.zeros(self.z_sz) # might not have value for all sliders
+        for i,slider in enumerate(self.sliders):
+            z[i]=slider.get()
+        newImg = npToTk(self.f(z))
+        self.walked.configure(image=newImg)
+        self.walked.image=newImg # prevent garbage collection
 
-    def visualize():
+    def visualize(self):
         ''' start visualization '''
         # init sliders
         frame=Frame(self.root)
@@ -52,13 +49,13 @@ class Visualizer:
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
 
-        for i in range(min(len(mu), max_args)): # XXX limited number of sliders-- fix this
-            l = Label(frame, text='Dim %s:'%i)
+        for i in range(min(self.z_sz, self.max_sliders)):
+            l = Label(frame, text='Dim %d:'%i)
             l.grid(row=i,column=0,sticky='w')
             w = Scale(
-                frame, from_=mu[i]-sigma[i], to=mu[i]+sigma[i],
-                orient=HORIZONTAL, length=300, command=refresh)
-            w.set(mu[i])
+                frame, from_=-self.window_sz, to=self.window_sz,
+                orient=HORIZONTAL, length=300, command=self.refresh)
+            w.set(0)
             w.grid(row=i,column=1,sticky='e')
             self.sliders.append(w)
 
